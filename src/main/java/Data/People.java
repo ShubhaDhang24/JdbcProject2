020c22cf9f69588587b9f737e9e639755435b0d6
Shubha Dhang <shubhadhang@gmail.com>
2023-09-27 09:35:56 +0200

    created class and established the connection to database

---
+++ src/main/java/Data/People.java
@@ -0,0 +1,21 @@
+package Data;
+
+import model.Person;
+
+import java.util.Collection;
+
+public interface People {
+
+    Person create(Person person);
+
+    Collection<Person> findAll();
+
+    Person findById(int id);
+    Collection<Person> findByName(String name);
+    Person update(Person person);
+    boolean deleteById(int id);
+
+
+}
+
+
---
+++ src/main/java/model/Person.java
@@ -0,0 +1,74 @@
+package model;
+
+import Data.People;
+
+import java.util.Collection;
+
+public class Person implements People {
+    private int person_id;
+    private String firstName;
+    private String lastName;
+
+    public Person( String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getPerson_id() {
+        return person_id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Person(int person_id, String firstName, String lastName) {
+        this.person_id = person_id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "person_id=" + person_id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public Person create(Person person) {
+        return person;
+    }
+
+    @Override
+    public Collection<Person> findAll() {
+
+        return null;
+    }
+
+    @Override
+    public Person findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Person> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Person update(Person person) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return false;
+    }
+}
